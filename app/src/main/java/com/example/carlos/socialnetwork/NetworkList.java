50c5f0f9543e33a565b04043f475beed72a14f8a
CarlosDubon <00058016@uca.edu.sv>
2018-04-14 12:26:09 -0600

    deteccion del error

--- app/src/main/java/com/example/carlos/socialnetwork/NetworkList.java
+++ app/src/main/java/com/example/carlos/socialnetwork/NetworkList.java
@@ -7,30 +7,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-
+import android.widget.ListView;
 
 
 public class NetworkList extends ListFragment implements AdapterView.OnItemClickListener {
 
     MainActivity mainActivity = new MainActivity();
+    ListView listView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_network_list,container,false);
+        listView = view.findViewById(R.id.list_network);
         return view;
     }
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
+    public void onActivityCreated( Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        /*Aqui se encuentra el error -comprobado-*/
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),android.R.layout.simple_list_item_1,mainActivity.getList_name_networks());
-        setListAdapter(adapter);
-        getListView().setOnItemClickListener(this);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
 
     }
 }
